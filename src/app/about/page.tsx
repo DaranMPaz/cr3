7a0f7354c8d890915ece06eb67c3e77a02a78dc1
Darlan Paz <rogue.one.dpaz@outlook.com>
2023-10-03 17:43:13 -0300

    adição de mais componentes na home

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,48 @@
-import Logo from '../../../public/logo-colored-light.png'
-import { HeaderContainer, HeaderLink, HeaderLinkGroup, HeaderLogo, HeaderButton, HeaderDropdown } from '@/app/components/header'
+import Logo from "../../../public/logo-colored-light.png";
+import {
+  HeaderContainer,
+  HeaderLink,
+  HeaderLinkGroup,
+  HeaderLogo,
+  HeaderButton,
+  HeaderDropdown,
+} from "@/app/components/header";
+import {
+  ButtonCtaRight,
+  ImageBackground,
+  PaddingContainer,
+  SectionContainer,
+  TextSection,
+} from "../components/section";
+import Banner1 from "../../../public/home/impact.png";
 
 export default function About() {
   return (
     <>
-    <HeaderContainer>
-      <HeaderLogo logo={Logo} />
-      <HeaderLinkGroup>
-        <HeaderLink link='/' text='Inicio' />
-        <HeaderLink link='/about' text='Conheça a CR3' isActive/>
-        <HeaderDropdown/>
-        <HeaderLink link='/news' text='Conteúdo' />
-        <HeaderLink link='/faq' text='Dúvidas' />
-        <HeaderButton link='/contact' text='Entre em Contato' />
-      </HeaderLinkGroup>
-    </HeaderContainer>
+      <HeaderContainer>
+        <HeaderLogo logo={Logo} />
+        <HeaderLinkGroup>
+          <HeaderLink link="/" text="Inicio" />
+          <HeaderLink link="/about" text="Conheça a CR3" isActive />
+          <HeaderDropdown />
+          <HeaderLink link="/news" text="Conteúdo" />
+          <HeaderLink link="/faq" text="Dúvidas" />
+          <HeaderButton link="/contact" text="Entre em Contato" />
+        </HeaderLinkGroup>
+      </HeaderContainer>
 
-      <h1>Sobre</h1>
-
+      {/* BANNER */}
+      <SectionContainer padding="none">
+        <PaddingContainer>
+          <TextSection
+            titleType="display"
+            br
+            title1="Quem Somos"
+            paragraph="Na CR3 Auditoria, nossa história é moldada por nossa paixão por excelência e nosso compromisso com a sustentabilidade."
+          />
+          <ImageBackground src={Banner1} alt="Banner1" />
+        </PaddingContainer>
+      </SectionContainer>
     </>
-  )
+  );
 }
