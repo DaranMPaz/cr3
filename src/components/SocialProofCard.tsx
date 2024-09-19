6eebbd7d524b858498ba55f1bad9cf0eea0724ab
DaramMPaz <rogue.one.dpaz@outlook.com>
2024-09-19 16:44:44 -0300

    Ajuste depoimentos

--- src/components/SocialProofCard.tsx
+++ src/components/SocialProofCard.tsx
@@ -5,28 +5,36 @@ export default function SocialProofCard(
     avatar,
     name,
     company,
-    comment
+    comment,
+    hasImage
   } : {
     avatar: any,
     name: string,
-    company: string,
-    comment: string
+    company?: string,
+    comment: string,
+    hasImage?: boolean
   }
 
 ) {
   return(
     <div className='flex flex-col gap-6 p-8 border-l-4 border-red-500 bg-zinc-800 w-full md:max-w-[373px]'>
 
-      <div className='flex items-center flex-1 gap-4'>
-        <Image src={avatar} alt='João Costa' height={48} className='rounded-full' />
+      <div className='flex items-center gap-4'>
+        { hasImage && <Image src={avatar} alt='João Costa' height={48} className='rounded-full' /> }
         <div className='flex w-full flex-col leading-tight'>
           <h4 className='font-bold text-xl '>{name}</h4>
-          <p className='text-zinc-400'>{company}</p>
+          { company && <p className='text-zinc-400'>{company}</p> }
         </div>
       </div>
 
       <div className='flex flex-1'>
-        <p className='text-zinc-300'>{comment}</p>
+        <p className='text-zinc-300 font-sans text-justify space-y-8'>
+          {comment.split("\n").map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}</p>
       </div>
     </div>
   )
--- src/components/SocialProofSection.tsx
+++ src/components/SocialProofSection.tsx
@@ -17,38 +17,41 @@ export default function SocialProofSection() {
             <SocialProofCard
               avatar={Avatar2}
               name='Ana Pereira'
-              company='Comércio Internacional Ltda.'
-              comment='"Implementar o novo serviço de auditoria da CR3 foi um divisor de águas para nossa empresa. A quantificação detalhada de retornos e a supervisão rigorosa dos dados de vendas nos ajudaram a melhorar nossa eficiência e a assegurar total conformidade com as recentes diretrizes normativas. Recomendamos fortemente!"'
+              comment={`excelente serviço prestado durante nossa auditoria de terceira parte. O trabalho realizado foi essencial para garantir a conformidade com as normas aplicáveis, ajudando-nos a identificar pontos de melhoria e assegurar a qualidade de nossos processos. \n
+              O conhecimento profundo da equipe sobre a legislação e a atenção aos detalhes nos proporcionaram resultados sólidos. Estamos confiantes em continuar essa parceria e contar com o suporte da CR3 em futuras auditorias.`}
             />
           </div>
 
           <SocialProofCard
             avatar={Avatar1}
-            name='Maria Silva'
-            company='Logística Global'
-            comment='"A CR3 Auditoria transformou a forma como gerenciamos nossas operações de auditoria. Com a nova abordagem, nossa empresa não só alcançou uma conformidade impecável com as normas recentes, mas também otimizou significativamente nossos processos logísticos. O relatório final foi detalhado e extremamente útil para nossas decisões estratégicas."'
+            name='Recicla Aqui'
+            comment={`Gostaríamos de agradecer à equipe da CR3 Auditoria pelo excelente trabalho realizado em nossa auditoria de terceira parte.\n
+              O processo foi super construtivo, fez a gente ajustar nossos processos e reorganizar toda a documentação, tanto nossa quanto dos operadores. No final, tudo deu certo e o resultado foi excelente! \n 
+              A equipe da CR3 é realmente muito competente e parece conhecer cada detalhe da legislação. \n 
+              Um ponto positivo é que o relatório foi aprovado de primeira pelo MMA, sem nenhuma solicitação de ajuste. Estamos muito felizes com essa parceria!`}
           />
 
           <SocialProofCard
             avatar={Avatar2}
-            name='Ana Pereira'
-            company='Comércio Internacional Ltda.'
-            comment='"Implementar o novo serviço de auditoria da CR3 foi um divisor de águas para nossa empresa. A quantificação detalhada de retornos e a supervisão rigorosa dos dados de vendas nos ajudaram a melhorar nossa eficiência e a assegurar total conformidade com as recentes diretrizes normativas. Recomendamos fortemente!"'
+            name='ILOG - Instituto Brasileiro de Logística Reversa'
+            comment={`Gostaríamos de parabenizar os auditores e a equipe pela contribuição e suporte que nos forneceram referente as auditorias obrigatórias que as Entidades Gestoras necessitam cumprir conforme os decretos, atendendo ao Decreto do IMASUL ao Decreto Federal para o MMA. \n Resultados positivos provém de excelentes trabalhos e desempenho. \n
+            Esperamos continuar nossa parceria para trabalhos futuros, na próxima entrega de Relatórios Comprovatórios aos órgãos ambientais estaduais e ao MMA – Ministério de Meio Ambiente.`}
           />
 
           <SocialProofCard
             avatar={Avatar3}
             name='João Costa'
-            company='Indústria de Embalagens'
-            comment='"O serviço de auditoria da CR3 foi essencial para nossa adaptação às novas regulamentações. A análise minuciosa das embalagens e a verificação precisa das notas fiscais garantiram que estivéssemos em total conformidade. A equipe foi extremamente profissional e o suporte foi além das expectativas."'
+            comment={`Equipe dedicada e com excelente suporte oferecido no cumprimento das auditorias obrigatórias que a nossa organização deve seguir. O atendimento às exigências estabelecidas foi realizado de forma precisa e eficiente. \n
+            O sucesso dos resultados obtidos reflete a excelência do trabalho realizado por todos os envolvidos. \n 
+            Estamos confiantes e esperamos continuar essa parceria na entrega de relatórios futuros aos órgãos competentes, garantindo a conformidade com todas as exigências normativas.`}
           />
 
           <div className='absolute top-[202px] -right-[25rem] md:opacity-20 opacity-0 hidden md:block'>
             <SocialProofCard
               avatar={Avatar1}
               name='Maria Silva'
-              company='Logística Global'
-              comment='"A CR3 Auditoria transformou a forma como gerenciamos nossas operações de auditoria. Com a nova abordagem, nossa empresa não só alcançou uma conformidade impecável com as normas recentes, mas também otimizou significativamente nossos processos logísticos. O relatório final foi detalhado e extremamente útil para nossas decisões estratégicas."'
+              comment={`Agradecemos à CR3 Auditoria pelo suporte eficiente durante nossa auditoria de terceira parte. A equipe demonstrou um alto nível de profissionalismo, ajudando-nos a garantir o cumprimento das normas regulatórias e a otimização dos nossos processos internos. \n 
+              O relatório final foi detalhado e aprovado rapidamente, sem necessidade de ajustes, evidenciando a competência da CR3. Estamos satisfeitos com o resultado e esperamos continuar contando com seus serviços em futuras auditorias.`}
             />
           </div>
 
